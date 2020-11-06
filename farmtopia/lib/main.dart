2d0ffd732928e2fc15a131d8374c427e093dbd8e
Chi Nguyen <phuongchi2782001@gmail.com>
2020-11-06 10:02:04 -0500

    'installing firebase_core'

--- farmtopia/lib/main.dart
+++ farmtopia/lib/main.dart
@@ -1,8 +1,39 @@
 import 'package:flutter/material.dart';
 //import 'package:farmtopia/pages/home.dart';
 import 'package:farmtopia/pages/login.dart';
+import 'package:farmtopia/pages/loading.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        /* // Check for errors
+        if (snapshot.hasError) {
+          return SomethingWentWrong();
+        } */
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Loading();
+      },
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
---
+++ farmtopia/lib/pages/loading.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0xFFF3DB74),
+        body: Center(
+          child: Text('Loading'),
+        ));
+  }
+}
